1d2d49b0dfc65817bb736db65e362f1c41ea46f8
Armlec <alasdairmccabe@gmail.com>
2023-02-15 15:42:20 +0000

    Removed deployement.sh, function.zip, query_totesy_db.py, and requirements from branch

--- query_totesy_db.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def lambda_handler(event, context):
-    logging.error("Error")
-    print('Lambda function executed!')
-    print("Event:", event)
-    print("Context:", context)
-    raise Exception("This is an error message")
-
