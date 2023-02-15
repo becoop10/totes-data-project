ad8c2e65a7fb03139066f68d7977c296e987f63e
Armlec <alasdairmccabe@gmail.com>
2023-02-15 15:19:53 +0000

    Created sns topic, topic subscription, cloudwatch log metric filter, and cloudwatch metric metric alarm. Confirmed emails are sent when errors appear

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,3 +1,27 @@
+resource "aws_sns_topic" "test_error_alerts" {
+  name = "test-error-alerts"
+  }
+
+  resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.test_error_alerts.arn
+  protocol = "email"
+  endpoint = local.emails # email in stored in local.tf
+}
+
+resource "aws_cloudwatch_log_group" "ingest_lambda_log_group" {
+  name = "/aws/lambda/${var.ingest_lambda_name}"
+  }
+
+resource "aws_cloudwatch_log_metric_filter" "any_error" {
+    name = "any_error_notification"
+    pattern = "ERROR"
+    log_group_name = "/aws/lambda/${var.ingest_lambda_name}"
+    metric_transformation {
+      name = "error_metric"
+      namespace = "ErrorMetrics"
+      value = "1"
+    }
+}
 
 resource "aws_cloudwatch_metric_alarm" "alert_errors" {
     alarm_name = "error_alert"
@@ -10,7 +34,7 @@ resource "aws_cloudwatch_metric_alarm" "alert_errors" {
     threshold = "1"
     alarm_description = "Metric filter activates alarm when error found"
     actions_enabled = "true"
-    alarm_actions = ["arn:aws:sns:us-east-1:699459409711:test-error-alerts"]
+    alarm_actions = [aws_sns_topic.test_error_alerts.arn]
     insufficient_data_actions = []
     treat_missing_data = "ignore"
 }
