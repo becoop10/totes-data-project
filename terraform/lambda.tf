d58f3da945de2d21aa92fb68067d9564166e2ae9
Armlec <alasdairmccabe@gmail.com>
2023-02-16 14:55:38 +0000

    Modified lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "ingest_lambda" {
-    filename = "../function.zip" # insert zip folder name here
+    filename = "../src/query_totesys_db" # insert zip folder name here
     function_name = "${var.ingest_lambda_name}"
     role = aws_iam_role.ingest_lambda_role.arn
     handler = "query_totesy_db.lambda_handler"
