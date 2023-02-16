4b34603631faba56976362be6165f06372763265
Armlec <alasdairmccabe@gmail.com>
2023-02-16 16:25:23 +0000

    Changed aws version from 4.52.0 to 4.54.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "4.54.0"
     }
   }
   required_version = ">= 1.1.0"
