c2e01c58f39820c62feb77221ea4f4367e982c1a
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-02-16 15:37:42 +0000

    altered s3 bucket names

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "ingested_data" {
-    bucket = "totes-amazeballs-s3-ingested-data-bucket-alasdair"
+    bucket = "totes-amazeballs-s3-ingested-data-bucket-alasdair-1234567"
 }
 
 resource "aws_s3_bucket" "processed_data" {
-    bucket = "totes-amazeballs-s3-processed-data-bucket-alasdair"
+    bucket = "totes-amazeballs-s3-processed-data-bucket-alasdair-1234567"
 }
