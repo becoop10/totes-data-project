b562549e1cf1c543fd773bfc01597caf43c7781a
Armlec <alasdairmccabe@gmail.com>
2023-02-17 13:57:39 +0000

    Added lambda invocation to lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,18 @@
 resource "aws_lambda_function" "ingest_lambda" {
-    filename = "../src/test_lambda.zip" # insert zip folder name here
+    filename = "../src/query_totesys_db.zip" # insert zip folder name here
     function_name = "${var.ingest_lambda_name}"
     role = aws_iam_role.ingest_lambda_role.arn
-    handler = "test_lambda.lambda_handler"
+    handler = "query_totesy_db.lambda_handler"
     runtime = "python3.9"
 }
 
+data "aws_lambda_invocation" "example" {
+  function_name = aws_lambda_function.ingest_lambda.function_name
+
+  input = <<JSON
+{
+  "key1": "value1",
+  "key2": "value2"
+}
+JSON
+}
