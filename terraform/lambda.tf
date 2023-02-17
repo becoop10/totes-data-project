06b348feabba785a4062e5b2d17fbed60802e50e
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-02-17 14:19:39 +0000

    removed invocation

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,14 +5,3 @@ resource "aws_lambda_function" "ingest_lambda" {
     handler = "query_totesy_db.lambda_handler"
     runtime = "python3.9"
 }
-
-data "aws_lambda_invocation" "example" {
-  function_name = aws_lambda_function.ingest_lambda.function_name
-
-  input = <<JSON
-{
-  "key1": "value1",
-  "key2": "value2"
-}
-JSON
-}
