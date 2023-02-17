5d61acebda475781fe170f47ab8231b0acd75ef3
Armlec <alasdairmccabe@gmail.com>
2023-02-17 11:33:39 +0000

    Testing terraform with test lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "ingest_lambda" {
-    filename = "../src/query_totesys_db.zip" # insert zip folder name here
+    filename = "../src/test_lambda.zip" # insert zip folder name here
     function_name = "${var.ingest_lambda_name}"
     role = aws_iam_role.ingest_lambda_role.arn
-    handler = "query_totesy_db.lambda_handler"
+    handler = "test_lambda.lambda_handler"
     runtime = "python3.9"
 }
 
