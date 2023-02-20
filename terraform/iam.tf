a48523c689e5f7b1ecb81e4cd5a1e0ec473ecb49
Markipooos <79716066+Markipooos@users.noreply.github.com>
2023-02-20 14:42:57 +0000

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "ingest_lambda_role" {
     name_prefix = "role-${var.ingest_lambda_name}"
     assume_role_policy = <<EOF
-    {
+{
         "Version": "2012-10-17",
         "Statement": [
             {
@@ -22,7 +22,7 @@ resource "aws_iam_role" "ingest_lambda_role" {
 
 data "aws_iam_policy_document" "s3_document" {
     statement {
-        actions = ["s3:PutObject"]
+        actions = ["s3:PutObject","s3:GetObject"]
 
         resources = [
             "${aws_s3_bucket.ingested_data.arn}/*"
@@ -69,4 +69,24 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
 resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
     role = aws_iam_role.ingest_lambda_role.name
     policy_arn = aws_iam_policy.cw_policy.arn
+}
+
+data "aws_iam_policy_document" "s3_fetches_document" {
+    statement {
+        actions = ["s3:ListAllMyBuckets"]
+
+        resources = [
+            "arn:aws:s3:::*"
+        ]
+    }
+}
+
+resource "aws_iam_policy" "s3_fetches_policy" {
+    name_prefix = "s3-fetches-policy-${var.ingest_lambda_name}"
+    policy = data.aws_iam_policy_document.s3_fetches_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_fetch_policy_attachment" {
+    role = aws_iam_role.ingest_lambda_role.name
+    policy_arn = aws_iam_policy.s3_fetches_policy.arn
 }
