bdb92f1fd89c9da9e47e53ea7bcbb6da271675c4
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-02-21 12:19:40 +0000

    environment variables set

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,13 @@ terraform {
     region = "us-east-1"
 
     # both access and secret keys will need to be changed for github secrets once that is working
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    # access_key = "${var.aws_access_key}"
+    # secret_key = "${var.aws_secret_key}"
   }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,10 +4,5 @@ variable "ingest_lambda_name" {
     default = "ingest-data"
 }
 
-variable "aws_access_key" {
-  type = string
-}
-
-variable "aws_secret_key" {
-  type = string
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
