a8d6438ad1eba6aa6f08c4ca080f3d30898ab83d
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-02-21 10:24:47 +0000

    initialised remote s3 state bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,7 @@ resource "aws_s3_bucket" "ingested_data" {
 resource "aws_s3_bucket" "processed_data" {
     bucket = "totes-amazeballs-s3-processed-data-bucket"
 }
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+    bucket = "totes-amazeballs-s3-terraform-state-bucket"
+}
