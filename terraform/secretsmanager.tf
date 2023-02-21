1ac5eb2f8c376c59db5607c2ee9bb3b1ad86e708
Armlec <alasdairmccabe@gmail.com>
2023-02-21 12:17:33 +0000

    Terraform.yml now uses environment variables set by github secrets. Added secretsmanager.tf, fixed deployment.zip, added secret policies to lambda.

---
+++ terraform/secretsmanager.tf
@@ -0,0 +1,20 @@
+variable "TOTESYS_DB_HOST" {}
+variable "TOTESYS_DB_PORT" {}
+variable "TOTESYS_DB_DATABASE" {}
+variable "TOTESYS_DB_USER" {}
+variable "TOTESYS_DB_PASSWORD" {}
+
+resource "aws_secretsmanager_secret" "totesys-db-credentials-4" {
+    name        = "totesys-db-credentials-4"
+}
+
+resource "aws_secretsmanager_secret_version" "example" {
+    secret_id     = aws_secretsmanager_secret.totesys-db-credentials-4.id
+    secret_string = jsonencode({
+        host: "${var.TOTESYS_DB_HOST}"
+        port: "${var.TOTESYS_DB_PORT}"
+        dbname: "${var.TOTESYS_DB_DATABASE}"
+        username: "${var.TOTESYS_DB_USER}"
+        password: "${var.TOTESYS_DB_PASSWORD}"          
+    })
+}
