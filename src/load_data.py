696ad6a98a14e0d2446b2b2f34fb27069cacab99
Armlec <alasdairmccabe@gmail.com>
2023-02-22 16:11:58 +0000

    Wrapped key and values in tuple()

--- src/load_data.py
+++ src/load_data.py
@@ -118,7 +118,7 @@ def query_builder(r, filename):
     values = [r[k] for k in keys]
     update_strings = [f'{k} = EXCLUDED.{k}' for k in keys ]
     full_update_string = ", ".join(update_strings)
-    var_in = (keys, values)
+    var_in = (tuple(keys), tuple(values))
     query = f'INSERT INTO {filename} (%s) VALUES (%s) ON CONFLICT ({id_columns[filename]}) DO UPDATE SET {full_update_string};'
     return query, var_in
 
