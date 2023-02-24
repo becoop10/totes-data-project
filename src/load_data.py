ab1ed4ee6a60abc114181e03742b372aa6873531
Armlec <alasdairmccabe@gmail.com>
2023-02-24 09:58:01 +0000

    Changed load timeout and lamda

--- src/load_data.py
+++ src/load_data.py
@@ -80,7 +80,7 @@ def read_parquets(s3, path, bucket):
     try:
         file=pd.read_parquet(BytesIO(response['Body'].read()))
         file=file.replace(pd.np.nan, None)
-        return file.to_dict('records')
+        return file
     except Exception as e:
         logger.error(e)
         logger.error(f'An error occured. Could not read parquet. Bucket: {bucket}, Path: {path}')
@@ -114,7 +114,7 @@ id_columns = {
     'fact_payment' : 'payment_id'
 }
 
-def query_builder(r, filename,invocations):
+def query_builder(r, filename):
     keys = list(r.keys())
     values = [r[k] for k in keys]
     update_strings = [f'{k} = EXCLUDED.{k}' for k in keys ]
@@ -123,12 +123,7 @@ def query_builder(r, filename,invocations):
     full_fact_update_string = ", ".join(fact_update_strings)
     key_string = ", ".join(keys)
 
-    if invocations == 0:
-        var_in=(tuple(values),)
-        query = f'INSERT INTO {filename} ({key_string}) VALUES %s;'
-
-
-    elif 'dim' in filename:
+    if 'dim' in filename:
         var_in = (tuple(values), )
         query = f'INSERT INTO {filename} ({key_string}) VALUES %s ON CONFLICT ({id_columns[filename]}) DO UPDATE SET {full_update_string};'
     else:
@@ -152,7 +147,7 @@ def query_builder(r, filename,invocations):
 
 def data_sorter(data, filename):
     id = id_columns[filename]
-    return sorted(data, key=lambda a: a[id] )
+    return data.sort_values(id)
 
 def get_file_names(bucket_name,prefix):
     s3 = boto3.client('s3')
@@ -181,13 +176,6 @@ def lambda_handler(event, context):
         response=0
     response=int(response)
 
-
-
-
-
-
-
-
     for key in list(id_columns.keys()):
         for f in updated_files:
             if key in f:
@@ -197,9 +185,19 @@ def lambda_handler(event, context):
                     if file == f'{f}':
                         data = read_parquets(s3, file, bucket)
                         sorted_data = data_sorter(data, filename)
-                        for r in sorted_data:      
-                            query, var_in = query_builder(r, filename,response)
-                            write_to_db(conn, query, var_in)
+                        if response == 0:
+                            out_buffer = BytesIO()
+                            sorted_data.to_csv(out_buffer, index=False)
+                            with conn.cursor() as cur:
+                                try:
+                                    curr.copy_from(out_buffer, filename)
+                                except Exception as e:
+                                    logger.error(e)
+                        else:
+                            for r in sorted_data.to_dict('records'):      
+                                query, var_in = query_builder(r, filename,response)
+                                write_to_db(conn, query, var_in)
+
                         logger.info(f'{f} uploaded to warehouse.')
     response+=1
 
