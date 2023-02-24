8219eb8064039706526935b7a53619ae61d87a29
Armlec <alasdairmccabe@gmail.com>
2023-02-24 14:23:21 +0000

    Back to basic SQL query and transform lambda is makin updates csv quickfire

--- src/load_data.py
+++ src/load_data.py
@@ -1,13 +1,12 @@
 import psycopg2
 import boto3
 import pandas as pd
-from io import BytesIO
+from io import BytesIO , StringIO
 from botocore.exceptions import ClientError
 import json
 import logging
 logger = logging.getLogger('WarehouseUploaderLogger')
 logger.setLevel(logging.INFO)
-from sqlalchemy import create_engine
 
 def get_secret():
 
@@ -115,7 +114,7 @@ id_columns = {
     'fact_payment' : 'payment_id'
 }
 
-def query_builder(r, filename):
+def query_builder(r, filename, invocations):
     keys = list(r.keys())
     values = [r[k] for k in keys]
     update_strings = [f'{k} = EXCLUDED.{k}' for k in keys ]
@@ -124,7 +123,12 @@ def query_builder(r, filename):
     full_fact_update_string = ", ".join(fact_update_strings)
     key_string = ", ".join(keys)
 
-    if 'dim' in filename:
+    if invocations == 0:
+        var_in = (tuple(values), )
+        query = f'INSERT INTO {filename} ({key_string}) VALUES %s;'
+
+
+    elif 'dim' in filename:
         var_in = (tuple(values), )
         query = f'INSERT INTO {filename} ({key_string}) VALUES %s ON CONFLICT ({id_columns[filename]}) DO UPDATE SET {full_update_string};'
     else:
@@ -168,14 +172,6 @@ def lambda_handler(event, context):
 
     lam=boto3.client('lambda')
 
-    db = get_secret()
-    host = db['host']
-    username = db['username']
-    password = db['password']
-    database = db['dbname']
-    port = db['port']
-    engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}/{database}')
-
     result=lam.get_function_configuration(
     FunctionName='load-data'
     )
@@ -195,21 +191,12 @@ def lambda_handler(event, context):
                     if file == f'{f}':
                         data = read_parquets(s3, file, bucket)
                         sorted_data = data_sorter(data, filename)
+                        sorted_data = sorted_data.to_dict('records')
+
                         response = 0
-                        if response == 0:
-                            sorted_data.to_sql(f'{filename}', engine, if_exists='append')
-                            # out_buffer = BytesIO()
-                            # sorted_data.to_csv(out_buffer, index=False)
-                            # with conn.cursor() as cur:
-                            #     try:
-                            #         cur.copy_from(out_buffer, filename)
-                            #     except Exception as e:
-                            #         logger.error(e)
-                            #         raise Exception
-                        else:
-                            for r in sorted_data.to_dict('records'):      
-                                query, var_in = query_builder(r, filename)
-                                write_to_db(conn, query, var_in)
+                        for r in sorted_data:      
+                            query, var_in = query_builder(r, filename, response)
+                            write_to_db(conn, query, var_in)
 
                         logger.info(f'{f} uploaded to warehouse.')
     response+=1
