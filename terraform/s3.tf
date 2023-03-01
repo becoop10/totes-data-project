de1d2cc4f1f70eed639bc02f2d4ceed3342ca97e
Armlec <alasdairmccabe@gmail.com>
2023-03-01 10:23:19 +0000

    Added initial timestamp using terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "ingested_data" {
 resource "aws_s3_bucket" "processed_data" {
     bucket_prefix = "totes-amazeballs-s3-processed"
 }
+
+resource "aws_s3_bucket_object" "initial_timestamp" {
+    bucket = aws_s3_bucket.ingested_data.id
+    key = "data/timestamp.txt"
+    source = "../timestamp.txt"
+}
