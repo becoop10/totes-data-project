4604573e75ca8fca0a4018c392ca825e398b7c57
Armlec <alasdairmccabe@gmail.com>
2023-03-01 17:34:08 +0000

    increasead ingest timeoue

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "ingest_lambda" {
     role = aws_iam_role.ingest_lambda_role.arn
     handler = "ingest_data.lambda_handler"
     runtime = "python3.9"
-    timeout = "10"
+    timeout = "20"
 }
 
 resource "aws_lambda_function" "transform_lambda" {
